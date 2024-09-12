6bee44b97f377e30af1105516298377739134431
Luka <datto0116@gmail.com>
2024-09-12 22:44:35 +0900

    Create Joystick.c

---
+++ Joystick.c
@@ -0,0 +1,29 @@
+### 조이스틱 모듈 사용
+int Joy_x = A0;
+int Joy_y = A1;
+int Joy_z = 8;
+
+void setup() {
+  pinMode(Joy_z, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int x = analogRead(Joy_x);
+  int y = analogRead(Joy_y);
+  int z = digitalRead(Joy_z);
+  Serial.print("X: ");
+  Serial.print(x);
+  Serial.print(" Y: ");
+  Serial.print(y);
+  Serial.print(" Z: ");
+  Serial.print(z);
+
+  if( z==0 ){
+    Serial.println("    Pressed");
+  }
+  else{
+    Serial.println("    Nothing");
+  }
+  delay(500);
+}
