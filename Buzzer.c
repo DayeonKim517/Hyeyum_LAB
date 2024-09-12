0195f54ff1186eacdc628cd945c1cd2d432191b4
Luka <datto0116@gmail.com>
2024-09-13 00:21:01 +0900

    Create Buzzer.c

---
+++ Buzzer.c
@@ -0,0 +1,17 @@
+# 부저
+int Buzzer = 7;
+
+void setup()
+{
+  pinMode(Buzzer, OUTPUT);
+}
+
+void loop()
+{
+  tone(Buzzer, 523);
+  delay(500);
+  tone(Buzzer, 587);
+  delay(500);
+  tone(Buzzer, 659);
+  delay(500);
+}
